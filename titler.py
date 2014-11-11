2f43a530ea6e42eee1f91ac0a1bd40d04c4b31e5
RexLi <r030515495@gmail.com>
2014-11-11 11:20:53 +0800

    新增chardet modules 解決遇到big5 網頁讀出來會有的問題

--- titler.py
+++ titler.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import urllib2,Alfred,sys;
 from bs4 import BeautifulSoup
+from chardet import *
 
 # query = sys.argv[1]
 
@@ -16,9 +17,13 @@ def getUrl(url):
         req = urllib2.Request(url, headers={'User-Agent' : "Magic Browser"})
         response = urllib2.urlopen(req)
         html = response.read()
+        # chardetdict=chardet.detect(html)
+        # if chardetdict.get('encoding')=='Big5':
+        #     html=html.decode('big5','ignore').encode('utf-8','ignore')
         response.close()
         soup = BeautifulSoup(html)
-        return "- [{0}]({1})".format(soup.title.contents[0].strip(), response.geturl())
+        title = soup.title.contents[0].strip()
+        return "- [{0}]({1})".format(title, response.geturl())
     except urllib2.URLError, e:
         return False
     except urllib2.HTTPError, e:
@@ -33,6 +38,8 @@ result = getUrl(handler.query)
 # test
 # not have html tag
 # result = getUrl("http://learn.getchef.com/")
+# title 遇到big5 網頁亂碼
+# result = getUrl("http://www2.nsysu.edu.tw/csmlab/unix/vi_command.htm")
 
 if not result:
     handler.add_new_item(title="No find WebPage:(")
